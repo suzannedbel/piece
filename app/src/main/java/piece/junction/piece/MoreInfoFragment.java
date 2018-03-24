e7836136b72e05cfe746bf74c905752e6a1f536b
Suzanne D'Bel <suzannedbel@gmail.com>
2018-03-24 16:53:39 +0900

    add choose item pictures

---
+++ app/src/main/java/piece/junction/piece/MoreInfoFragment.java
@@ -0,0 +1,23 @@
+package piece.junction.piece;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Suzie on 24.03.18.
+ */
+
+public class MoreInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_moreinfo, container, false);
+
+        return rootView;
+    }
+}
